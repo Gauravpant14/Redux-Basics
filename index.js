7f25000042f374586b246ecf7dbe70b19ffd52bf
Gaurav <aadeezcity@gmail.com>
2021-03-22 15:26:46 +0530

    using single reducer for multiple actions

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const createStore = redux.createStore
 
 
 const BUY_CAKE = "BUY_CAKE"
-
+const BUY_ICECREAM = "BUY_ICECREAM "
 //define action --> An action is an object with type property
 
 // {
@@ -20,6 +20,11 @@ function buyCake() {
      }
 }
 
+function buyIceCream() {
+    return {
+        type: BUY_ICECREAM
+    }
+}
 
 // <--Implementing Reducer-->
 //(previousState, action) => newState
@@ -27,7 +32,8 @@ function buyCake() {
 //state of application
 
 const initialState = {
-    numOfCakes : 10 
+    numOfCakes : 10, 
+    numOfIceCreams: 20
 }
 
 // <--Implementing Reducer-->
@@ -38,6 +44,11 @@ const reducer = (state = initialState, action) => {
         case BUY_CAKE: return {
             ...state, 
             numOfCakes: state.numOfCakes - 1
+        }
+
+        case BUY_ICECREAM: return {
+            ...state,
+            numOfIceCreams: state.numOfIceCreams - 1
         }
         default:return state
     }
@@ -54,5 +65,8 @@ const unsubscribe = store.subscribe(() => console.log('Updated state', store.get
 store.dispatch(buyCake()) //dispatch method accepts a action method as an parameter
 store.dispatch(buyCake()) // Allow state to be updated via dispatch(action)
 store.dispatch(buyCake()) 
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
 
 unsubscribe()
