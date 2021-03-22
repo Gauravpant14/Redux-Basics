78241cea037c7de1c3786053b06bb08fd6064f40
Gaurav <aadeezcity@gmail.com>
2021-03-22 15:12:15 +0530

    redux use with single reducer

---
+++ index.js
@@ -0,0 +1,58 @@
+const redux = require('redux')
+const createStore = redux.createStore
+
+
+const BUY_CAKE = "BUY_CAKE"
+
+//define action --> An action is an object with type property
+
+// {
+//    type:BUY_CAKE ,
+//    info:'First redux action'
+// }
+
+//Action Creator ---> is a function That returns the action
+
+function buyCake() {
+    return {
+        type:BUY_CAKE ,
+        info:'First redux action'
+     }
+}
+
+
+// <--Implementing Reducer-->
+//(previousState, action) => newState
+
+//state of application
+
+const initialState = {
+    numOfCakes : 10 
+}
+
+// <--Implementing Reducer-->
+
+//reducer function which controls how the state transtion happens
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case BUY_CAKE: return {
+            ...state, 
+            numOfCakes: state.numOfCakes - 1
+        }
+        default:return state
+    }
+}
+
+
+
+const store = createStore(reducer) //redux store holding the application state
+console.log("initial state", store.getState()); //allowing access to the state via getState()
+
+// store.subscribe(() => console.log('Updated state', store.getState())) //Allow the app to subscribe to changes in the store ,that is acchieved by using subscribe method
+
+const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()))
+store.dispatch(buyCake()) //dispatch method accepts a action method as an parameter
+store.dispatch(buyCake()) // Allow state to be updated via dispatch(action)
+store.dispatch(buyCake()) 
+
+unsubscribe()
